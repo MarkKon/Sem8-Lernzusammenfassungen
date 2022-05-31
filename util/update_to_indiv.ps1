cd558b023e00065a7404a65260da19b932adc7ec
MARKKon <konstantin.mark99@gmail.com>
2022-05-31 11:25:02 +0200

    Some verrrrry wonky code....

--- util/update_to_indiv.ps1
+++ util/update_to_indiv.ps1
@@ -9,3 +9,13 @@ $Repos | Write-Host
 $Folders | ForEach-Object {
   Copy-Item -Path "$_\*" -Destination "..\Sem8-$_\sum" -Recurse
 }
+
+$Folders | ForEach-Object {
+  Set-Location "..\Sem8-$_"
+  git pull
+  git pull overleaf master
+  git add "sum/zus.tex"
+  git commit -m "Moved from Repo Sem8-Lernzusammenfassungen"
+  ./util/push_both.ps1
+}
+cd "..\Sem8-Lernzusammenfassungen"
